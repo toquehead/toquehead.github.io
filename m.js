544335724071b83e416451d146ff8ec79797206e
Doug Brown <toquesoft@gmail.com>
2016-10-12 17:05:55 +1100

    Mobile testing

---
+++ m.js
@@ -0,0 +1,9 @@
+/// <reference path="scripts/jquery-2.2.0.min.js" />
+/// <reference path="oz-links.js" />
+"use strict";
+
+alert('javascript');
+
+$(document).ready(function () {
+    alert('jQuery');
+});
